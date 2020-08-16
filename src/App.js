a4ffaae0407e02cd0866ff16b4ebf1e4648506df
Amal <amal@amallevi.com>
2020-08-15 20:59:10 -0700

    'Second Commit'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import {Router} from "@reach/router";
+import Contact from "./components/Contact";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <Router>
+        <Home path={'/'}/>
+        <About path={'/about'}/>
+        <Contact path={'/contact'}/>
+      </Router>
+  )
 }
 
 export default App;
