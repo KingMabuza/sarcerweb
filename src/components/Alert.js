e069730c30c49b863c37409482312e058e7a30fc
Amal <amal@amallevi.com>
2020-08-20 17:30:15 -0700

    'tenth Commit'

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Nav from "./Nav";
+import Footer from "./Footer";
 
 function Alert() {
     return(
         <div>
-            <Nav/>
+            <div className={'row'}>
+                <div className={'col s2'}>
+                    <Nav/>
+                </div>
+                <div className={'col s10 center homeContent'}>
+                    <h3>SARCER</h3>
+                    <p>Thank You. Your Message Has Been Received</p>
+                    <Footer/>
+                </div>
+            </div>
         </div>
         )
 }
